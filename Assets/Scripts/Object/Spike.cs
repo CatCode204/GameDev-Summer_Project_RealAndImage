7d5a25773530f9909739c0d5ff5e55c76d0dfbfc
TruongTamAn2004 <truongtaman2004@gmail.com>
2023-07-26 23:14:29 +0700

    Update Spike

--- Assets/Scripts/Object/Spike.cs
+++ Assets/Scripts/Object/Spike.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class Spike : MonoBehaviour {
     /* * * Constant Variables * * */
     const string PLAYER_TAG = "Player";
-    private void OnColliderEnter2D(Collision2D other) {
-        Debug.Log("Worked");
-        if (other.gameObject.CompareTag(PLAYER_TAG)) 
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag(PLAYER_TAG))
             StartCoroutine(PlayerController.instance.Spawn());
     }
 }
